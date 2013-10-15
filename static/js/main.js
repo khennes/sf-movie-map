ac4de1bf6f4d29bdbbf3de08c9c54893afa1f9b0
katherine <katherine.hennes@gmail.com>
2013-10-15 01:50:27 -0700

    Render map with all markers

--- static/js/main.js
+++ static/js/main.js
@@ -3,9 +3,11 @@
  **/
 
 function initialize() {
- 
-    var geocoder = new google.maps.Geocoder();
 
+    /**
+     * Create map instance
+     **/
+ 
     var mapOptions = {
         center: new google.maps.LatLng(37.775057, -122.419281),
         zoom: 13,
@@ -14,75 +16,93 @@ function initialize() {
 
     var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 
-    var BOUNDS = new google.maps.LatLngBounds(new google.maps.LatLng(37.693601037244406, -122.51541137695312), new google.maps.LatLng(37.83419172060043, -122.35542297363281));
-   
-    
-    /** 
-     * Use Google Maps Geocoder service to get lat/lng
-     * coords of first (assumed best) location search result;
-     * construct a new Marker object for each scene
-     *
-     * Only items with a location property will be displayed
+
+    /**
+     * Backbone models and views
+     **/
+
+    var Marker = Backbone.Model.extend({
+        defaults: {
+            title: null,
+            id: null,
+            year: null,
+            director: null,
+            position: null }
+    });
+
+    var MasterCollection = Backbone.Collection.extend({
+        model: Marker
+    });
+
+    var FilterCollection = Backbone.Collection.extend({
+        model: Marker
+    });
+
+
+    /**
+     * Given each scene/film site as an object, create an info window,
+     * Backbone model, and Maps marker for each.
      **/
 
     function setMarker(i, data) {
         var scene = data[i];
-        if (scene['locations']) {
+        if (scene['latlong']) {  // only jsonify valid locations before passing to client
 
-            geocoder.geocode( { 'address': scene['locations'], 'bounds': BOUNDS }, function(results, status) {
-                if (status == google.maps.GeocoderStatus.OK) {
+            var contentString = "<div class='content'>" +
+                "<div id='siteNotice'>" +
+                "</div>" +
+                "<h1 id='firstHeading' class='firstHeading'>" + 
+                scene['title'] + " (" + scene['release_year'] + ")</h1>" +
+                "<div id='bodyContent'>" +
+                "<p>Director: " + scene['director'] + "</p>" +
+                "<p>Location: " + scene['locations'] + "</p>" +
+                "</div>" +
+                "</div>";
 
-                    var contentString = "<div class='content'>" +
-                        "<div id='siteNotice'>" +
-                        "</div>" +
-                        "<h1 id='firstHeading' class='firstHeading'>" + 
-                        scene['title'] + " (" + scene['release_year'] + ")</h1>" +
-                        "<div id='bodyContent'>" +
-                        "<p>Director: " + scene['director'] + "</p>" +
-                        "<p>Location: " + scene['locations'] + "</p>" +
-                        "</div>" +
-                        "</div>";
+            if (scene['fun_facts']) {
+                var funFact = "<p>Fun fact: " + scene['fun_facts'] + "</p>";
+                contentString += funFact;
+            }
+                
+            var infoWindow = new google.maps.InfoWindow({
+                content: contentString
+            });
 
-                    if (scene['fun_facts']) {
-                        var funFact = "<p>Fun fact: " + scene['fun_facts'] + "</p>";
-                        contentString += funFact;
-                    }
-                        
-                    var infoWindow = new google.maps.InfoWindow({
-                        content: contentString
-                    });
+            
+            /**
+             * Place marker for each scene/location in JSON array
+             * Add marker to MasterCollection of markers (Backbone model)
+             * Star icons courtesy of http://mapicons.nicolasmollet.com/
+             **/
 
-                    var marker = new google.maps.Marker({
-                        map: map,
-                        position: results[0].geometry.location,
-                        title: 'Movie'
-                    });
+            var marker = new google.maps.Marker({
+                map: map,
+                position: new google.maps.LatLng(scene['latlong'][0], scene['latlong'][1]),
+                icon: 'static/js/star-3.png'
+            });
 
-                    google.maps.event.addListener(marker, 'click', function() {
-                        infoWindow.open(map,marker);
-                    });
+            //MasterCollection.push([
+            //        { title: scene['title'],
+            //          id: i,
+            //          year: scene['release_year'],
+            //          director: scene['director'],
+            //          position: new google.maps.LatLng(scene['latlong']),
+            //          position: scene['latlong'] }
+            //]);
 
-                    console.log("marker at " + results[0].geometry.location + " aka " + scene['locations'] + " for " + scene['title']);
-
-                } else if (status == google.maps.GeocoderStatus.OVER_QUERY_LIMIT) {
-                    console.log("waiting...");
-                    window.setTimeout(function() {}, 2000);
-
-                } else {
-                    console.log("Geocode was unsuccessful for " + scene['title'] + status);
-                    console.log("Location: " + scene['locations']);
-                }
+            google.maps.event.addListener(marker, 'click', function() {
+                infoWindow.open(map,marker);
             });
         }
     }
 
 
     /** 
-     * Fetch array of JSON objects and assign each to a marker
+     * Fetch array of geocoded JSON objects and call setMarker() on each; 
      * Bias location results towards BOUNDS
      **/
 
-    $.getJSON('static/js/data.json', function(data) {
+    $.getJSON('static/js/geocoded.json', function(data) {
         var SCENES = data.length;
         for (var i = 0; i < SCENES; i++) {
             setMarker(i, data);
@@ -95,24 +115,11 @@ function initialize() {
 // marker.setMap(map);
 // marker.setMap(null);
 
-function hideDrawer() {
-    var navBox = document.getElementById('nav-box');
-}
-
-
-var Location = Backbone.Model.extend({
-    defaults: {
-        latitude: null,
-        longitude: null,
-        color: 'yellow'
-      }
-});
 
 function loadScript() {
     var script = document.createElement('script');
     script.src = "http://maps.googleapis.com/maps/api/js?key=AIzaSyAAHAi7BkDviZMKVSG1hr4lSkEAW2EWM9g&sensor=false&callback=initialize";
     document.body.appendChild(script);
 }
-
 
 window.onload = loadScript;
