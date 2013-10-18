93005291213d1dd44c2929c241171088c8de701b
katherine <katherine.hennes@gmail.com>
2013-10-17 18:39:07 -0700

    Reorganize to better conform to Backbone style; build out view

--- static/js/main.js
+++ static/js/main.js
@@ -1,128 +1,100 @@
-/**
- * Render the map and initially display all markers
- **/
-
 function initialize() {
+    'use strict';
 
-    /**
-     * Create map instance
-     **/
- 
-    var mapOptions = {
+    // Create map instance
+    var map = new google.maps.Map(document.getElementById('map-canvas'), { 
         center: new google.maps.LatLng(37.775057, -122.419281),
         zoom: 13,
         mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-
-    var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-
-
-    /**
-     * Backbone models and views
-     **/
-
-    var Marker = Backbone.Model.extend({
-        defaults: {
-            title: null,
-            id: null,
-            year: null,
-            director: null,
-            position: null }
     });
 
+    // Initialize Backbone MVC variables
+    var Marker = Backbone.Model.extend({});
     var Markers = Backbone.Collection.extend({
         model: Marker
-    });
+    });     
 
-    // create instance of Backbone collection
-    var allmarkers = new Markers();
+    var allMarkers = new Markers();
+    var allFilters = new Markers();
 
+    // object to store Google Maps markers
+    var markersList = {};
 
-    /**
-     * Given each scene/film site as an object, create an info window,
-     * Backbone model, and Maps marker for each.
-     **/
-
-    function setMarker(i, data) {
-        var scene = data[i];
-        if (scene['latlong']) {  // only jsonify valid locations before passing to client
-
-            var contentString = "<div class='content'>" +
-                "<div id='siteNotice'>" +
-                "</div>" +
-                "<h1 id='firstHeading' class='firstHeading'>" + 
-                scene['title'] + " (" + scene['release_year'] + ")</h1>" +
-                "<div id='bodyContent'>" +
-                "<p>Director: " + scene['director'] + "</p>" +
-                "<p>Location: " + scene['locations'] + "</p>" +
-                "</div>";
-
-            if (scene['fun_facts']) {
-                var funFact = "<p>Fun fact: " + scene['fun_facts'] + "</p>";
-                contentString += funFact;
-            }
-
-            contentString += "</div>";
-                
-            var infoWindow = new google.maps.InfoWindow({
-                content: contentString
-            });
-
-            
-            /**
-             * Place marker for each scene/location in JSON array
-             * Add marker to MasterCollection of markers (Backbone model)
-             * Star icons courtesy of http://mapicons.nicolasmollet.com/
-             **/
-
-            var marker = new google.maps.Marker({
-                map: map,
-                position: new google.maps.LatLng(scene['latlong'][0], scene['latlong'][1]),
-                icon: 'static/js/star-3.png',
-                zIndex: i
-            });
-
-            // create instance of Marker model and add to master collection
-            var newMarker = Marker.extend({
-                title: scene['title'],
-                id: i,
-                year: scene['release_year'],
-                director: scene['director'],
-                position: new google.maps.LatLng(scene['latlong'][0], scene['latlong'][1])
-            });
-
-            allmarkers.push(newMarker);
-
-            google.maps.event.addListener(marker, 'click', function() {
-                infoWindow.open(map,marker);
-            });
-        }
-    }
-
-
-    /** 
-     * Fetch array of geocoded JSON objects and call setMarker() on each; 
-     * Bias location results towards BOUNDS
-     *
-     * Populate local arrays for autocompletion search (filter options).
-     **/
-
-    
+    // Create info window and marker for each scene in JSON array, add to collection
+    // Populate arrays for autompletion/filtering
     $.getJSON('static/js/geocoded.json', function(data) {
         var SCENES = data.length;
 
-        var titles = {};
-        var directors = {};
-        var years = {};
-
         for (var i = 0; i < SCENES; i++) {
-            setMarker(i, data);
-            titles[data[i]['title']] = titles[data[i]['title']] + 1 || 1;
-            directors[data[i]['director']] = directors[data[i]['director']] + 1 || 1;
-            years[data[i]['release_year']] = years[data[i]['release_year']] + 1 || 1;
+            var scene = data[i];
+            if (scene['latlong']) {  // (TODO: only jsonify valid locations before passing to client)
+
+                // create info window, specify content
+                var contentString = "<div class='content'>" +
+                    "<div id='siteNotice'>" +
+                    "</div>" +
+                    "<h1 id='firstHeading' class='firstHeading'>" + 
+                    scene['title'] + " (" + scene['release_year'] + ")</h1>" +
+                    "<div id='bodyContent'>" +
+                    "<p>Director: " + scene['director'] + "</p>" +
+                    "<p>Location: " + scene['locations'] + "</p>" +
+                    "</div>";
+
+                if (scene['fun_facts']) {
+                    var funFact = "<p>Fun fact: " + scene['fun_facts'] + "</p>";
+                    contentString += funFact;
+                }
+
+                contentString += "</div>";
+                    
+                var infoWindow = new google.maps.InfoWindow({
+                    content: contentString,
+                    pixelOffset: 2
+                });
+
+                
+                /**
+                 * Create a marker for each scene, add Marker model to collection 
+                 * Star icons courtesy of http://mapicons.nicolasmollet.com/
+                 **/
+
+                // Google Maps marker
+                var marker = new google.maps.Marker({
+                    map: null,  // leave it to the MarkerView to hide/render the icon
+                    position: new google.maps.LatLng(scene['latlong'][0], scene['latlong'][1]),
+                    icon: 'static/js/star-3.png',
+                    zIndex: i
+                });
+
+                // my Backbone model marker
+                var newMarker = Marker.extend({
+                    title: scene['title'],
+                    id: i,  // Backbone marker id will match Maps marker zIndex
+                    year: scene['release_year'],
+                    director: scene['director'],
+                    position: new google.maps.LatLng(scene['latlong'][0], scene['latlong'][1])
+                });
+
+                markersList[i] = marker;
+                allMarkers.push(newMarker);
+
+                google.maps.event.addListener(marker, 'click', function() {
+                    // TODO: clicks away should close info window
+                    infoWindow.open(map,marker);
+                });
+            }
         }
 
-        // describe autocomplete options
+
+        /**
+         * Populate arrays for autocompletion options (for 'filter by' fields)
+         **/
+
+        var titles = _.pluck(allMarkers.models, 'title');
+        var directors = _.pluck(allMarkers.models, 'director');
+        var years = _.pluck(allMarkers.models, 'year');
+        // var locations = _.pluck(allMarkers, 'latlong');
+
         $("#by-title").typeahead({ 
             name: 'titles',
             local: Object.keys(titles)
@@ -138,57 +110,80 @@ function initialize() {
             local: Object.keys(years)
         });
 
-        // $("#by-location").typeahead({ local: locations });
+        // $("#by-location").typeahead({ 
+        //     name: 'locations',
+        //     remote:  
+        // });
     });
+    
+    console.log(allMarkers);
 
-    var title_query, director_query, year_query, location_query;
+    var MarkerView = Backbone.View.extend({
+        initialize: function() {
+            _.bindAll(this, 'showFilter', 'showAll', 'render');
+            this.showAll();
+        },
+        events: {
+            'click #go': 'showFilter',
+            'click #reset': 'showAll'
+        },
+        showFilter: function() {
+            var filter_options = {};
 
-    var filtered_title = allmarkers.get(title_query);
-    var filtered_director = allmarkers.get(director_query);
-    var filtered_year = allmarkers.get(year_query);
-    var filtered_location = allmarkers.get(location_query);
+            var title_query = $('#by-title').val();
+            var director_query = $('#by-director').val();
+            var year_query = $('#by-year').val();
+            // var location_query = $('#by-location').val();
 
-    /*
-    $('#go').click(function(event) {
 
-        // first, set each in allmarkers to null
-        // marker.setMap(null);
+            if (title_query != null) {
+                filter_options[title] = title_query;
+            }
 
-        var Filters = Backbone.Collection.extend({
-            model: Marker
-        });
+            if (director_query != null) {
+                filter_options[director] = director_query;
+            }
+        
+            if (year_query != null) {
+                filter_options[year] = year_query;
+            }
 
-        var allfilters = new Filters();
+            // TODO: location queries to be handled differently
 
-        var FilterView = Backbone.View.extend({
-            render: function() {
-               var matched_markers = [];
-                this.collection.each(function() {
-                    // render markers whose zIndex = Marker id
-                    // marker.setMap(map);
-                }
+            var match_filters = allMarkers.where(filter_options);
+
+            console.log(match_filters);
+
+            if (match_filters.length > 0) {
+                allFilters.add(match_filters);
+                this.render(allFilters);
+            } else {
+                $('#user-message').text('No results found');
+                this.render(this.collection);
+            }
+        },
+        showAll: function() {
+            this.render(this.collection);
+        },
+        render: function(collection) {
+            console.log(collection);
+            _.each(collection, function(el) {
+                console.log("call meeee");
+                markersList[el.id].setMap(map);
             });
-        });
-
-        var filtered_models = allmarkers.where({ 
-            title: filtered_title,
-            year: filtered_year,
-            director: filtered_director,
-            location: filtered_location
-        });
-
-        allfilters.add(filtered_models);
+        }
     });
-*/
 
-    $('#reset').click(function(event) {});
+    var markerView = new MarkerView({
+        collection: allMarkers
+    });
 }
 
 
 function loadScript() {
     var script = document.createElement('script');
     script.src = "http://maps.googleapis.com/maps/api/js?key=AIzaSyAAHAi7BkDviZMKVSG1hr4lSkEAW2EWM9g&sensor=false&callback=initialize";
-    document.body.appendChild(script);
+    $('body').append(script);
 }
 
-window.onload = loadScript;
+$('document').ready(loadScript);
