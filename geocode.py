e49fb529ca89519364c1bd1150141d4759161a11
katherine <katherine.hennes@gmail.com>
2013-10-20 20:27:58 -0700

    Add min & max variables instead of hard-coding boundaries

--- geocode.py
+++ geocode.py
@@ -9,12 +9,15 @@ def geocode_locations(filename):
 
     # bias location search results to SF
     # coordinates courtesy of http://jsfiddle.net/yV6xv/16/
-    BOUNDS = ((37.75334401310656, -122.45498657226562), (37.811411388625636, -122.39181518554688))
+    maxLat = 37.83473402375478
+    minLat = 37.59682400108367
+    maxLong = -122.35885620117188
+    minLong = -122.52433776855469
+    BOUNDS = ((minLat, minLong), (maxLat, maxLong))
 
     json_file = open(filename)
     data = json.load(json_file)
     errors = 0  # for debugging
-    address_string = ''
 
     for scene in data:
         if scene.get('latlong') != None:
@@ -24,9 +27,9 @@ def geocode_locations(filename):
                 if u'\xe9' in scene['locations']:  # filter for most common unicode error
                     scene['locations'] = scene['locations'].replace(u'\xe9', 'e')
 
-                # to capture actual addresses (usually in parens) for more accurate querying 
+                # to capture street addresses (usually in parens) for more accurate querying 
                 if '(' in scene['locations']:
-                    address_string = re.search('\((.*?)\)', scene['locations']).group(1)
+                    address_string = '%s, San Francisco, CA' % re.search('\((.*?)\)', scene['locations']).group(1)
                 else:
                     address_string = scene['locations']
 
@@ -34,12 +37,14 @@ def geocode_locations(filename):
 
                 # check that first result's coordinates are within vicinity of SF;
                 # if not, loop # through the remaining results
-                if not 37.0 < results[0].coordinates[0] < 38.0 or not -123.0 < results[0].coordinates[1] < -122.0:
+                if not minLat < results[0].coordinates[0] < maxLat or not minLong < results[0].coordinates[1] < maxLong:
                     if len(results) > 1:
                         for i in range(1, len(results)):
-                            if 37.0 < results[i].coordinates[0] < 38.0 and -123.0 < results[i].coordinates[1] < -122.0:
+                            if minLat < results[i].coordinates[0] < maxLat and minLong < results[i].coordinates[1] < maxLong:
                                 scene['latlong'] = results[i].coordinates
                                 break
+                        if scene.get('latlong') == None:
+                            errors += 1
                 else:
                     scene['latlong'] = results[0].coordinates
             except GeocoderError:
